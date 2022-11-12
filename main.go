993449a5b1f37b8cb125dfe43aee31df9354cf3c
akbari01 <akbari01.dev@gmail.com>
2022-11-12 14:58:00 +0430

    env file added

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"google.golang.org/grpc"
 
 	acl "github.com/ory/keto/proto/ory/keto/relation_tuples/v1alpha2"
 )
 
-func checkAcess() {
-	conn, err := grpc.Dial("88.208.199.201:4466", grpc.WithInsecure())
+func CheckAccess() {
+	conn, err := grpc.Dial(os.Getenv("KETO_READ_API"), grpc.WithInsecure())
 	if err != nil {
 		panic(err.Error())
 	}
@@ -18,7 +19,7 @@ func checkAcess() {
 	client := acl.NewCheckServiceClient(conn)
 
 	res, err := client.Check(context.Background(), &acl.CheckRequest{
-		Namespace: "shifts-namespace",
+		Namespace: os.Getenv("KETO_NAMESPACE"),
 		Object:    "02y_15_4w350m3",
 		Relation:  "decypher",
 		Subject:   acl.NewSubjectID("yaseen"),
@@ -34,8 +35,8 @@ func checkAcess() {
 	fmt.Println("Denied")
 }
 
-func main() {
-	conn, err := grpc.Dial("88.208.199.201:4467", grpc.WithInsecure())
+func GrantAccess() {
+	conn, err := grpc.Dial(os.Getenv("KETO_WRITE_API"), grpc.WithInsecure())
 	if err != nil {
 		panic("Encountered error: " + err.Error())
 	}
@@ -47,7 +48,7 @@ func main() {
 			{
 				Action: acl.RelationTupleDelta_ACTION_INSERT,
 				RelationTuple: &acl.RelationTuple{
-					Namespace: "shifts-namespace",
+					Namespace: os.Getenv("KETO_NAMESPACE"),
 					Object:    "02y_15_4w350m3",
 					Relation:  "decypher",
 					Subject:   acl.NewSubjectID("john"),
@@ -61,5 +62,9 @@ func main() {
 	}
 
 	fmt.Println("Successfully created tuple")
-	checkAcess()
+}
+
+func main() {
+	GrantAccess()
+	CheckAccess()
 }
